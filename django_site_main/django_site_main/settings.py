e3dde9e6d16cdc28bfc6ddf7d80928543a32eb49
Krzysztof <krisun17@gmail.com>
2015-05-13 14:36:01 +0200

    dodanie bazy lokalnej

--- django_site_main/django_site_main/settings.py
+++ django_site_main/django_site_main/settings.py
@@ -144,7 +144,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'polls',
+    #'polls',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
